3fb11074506e0c46202a96b146fdcd709e69356f
Milanzerg <55619925+milanzerg@users.noreply.github.com>
2022-05-01 23:30:01 +0300

    Add files via upload

--- new_erc20.sol
+++ new_erc20.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.4.24; //Declaring the solidity version
- 
+
+import "contracts/Owner.sol";
+import "hardhat/console.sol";
+
 //Safe Math Interface
  
 contract SafeMath { // Calling the Safe Math interface to use math functions in our contract.
@@ -49,7 +52,7 @@ contract ApproveAndCallFallBack { // A Contract function to receive approval and
  
 //Actual token contract
  
-contract TCtoken is ERC20Interface, SafeMath { 
+contract TCtoken is ERC20Interface, SafeMath, Owner { 
 
 /* Starting my TCtoken contract, creating a variable symbol of string type to
 hold my token’s symbol, a variable name of string type to hold my token’s name, 
@@ -78,76 +81,78 @@ variable decimals of unsigned integer type to hold the decimal value for the tok
         emit Transfer(address(0), 0x72b433a537D24dEC55a999435746E7b2f22848bA, _totalSupply);
     }
  
+    modifier Blocking {
+        require(
+            Block_var == false,
+            "All functions are blocked"    );
+        _;
+    }
+
+
  // Function totalSupply which will govern the total supply of our token.
-    function totalSupply() public constant returns (uint) {
-        if (Block_var != true) return _totalSupply  - balances[address(0)];
+    function totalSupply() public Blocking constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
     }
 
  //Function balanceOf which will check the balance of a wallet address.
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
-        if (Block_var != true) return balances[tokenOwner];
+    function balanceOf(address tokenOwner) public Blocking constant returns (uint balance) {
+        return balances[tokenOwner];
     }
  
  //Function transfer which will execute the transfer of tokens from the total supply to users.
-    function transfer(address to, uint tokens) public returns (bool success) {
-        if (Block_var != true) {
+    function transfer(address to, uint tokens) public Blocking returns (bool success) {
             balances[msg.sender] = safeSub(balances[msg.sender], tokens);
             balances[to] = safeAdd(balances[to], tokens);
             emit Transfer(msg.sender, to, tokens);
             return true;
         }
-    }
  
  //Function approve which will check if the total supply has the amount of token which needs to be allocated to a user.
-    function approve(address spender, uint tokens) public returns (bool success) {
-        if (Block_var != true) {
+    function approve(address spender, uint tokens) public Blocking returns (bool success) {
             allowed[msg.sender][spender] = tokens;
             emit Approval(msg.sender, spender, tokens);
             return true;
         }
-    }
  
  //Function transferFrom which will facilitate the transfer of token between users.
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        if (Block_var != true) {
+    function transferFrom(address from, address to, uint tokens) public Blocking returns (bool success) {
             balances[from] = safeSub(balances[from], tokens);
             allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
             balances[to] = safeAdd(balances[to], tokens);
             emit Transfer(from, to, tokens);
             return true;
         }
-    }
  
  //Function allowance which will check if a user has enough balance to perform the transfer to another user.
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-        if (Block_var != true) return allowed[tokenOwner][spender];
+    function allowance(address tokenOwner, address spender) public Blocking constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
     }
  
  //Function approveAndCall which executes the transactions of buying and spending of tokens.
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-        if (Block_var != true) {
+    function approveAndCall(address spender, uint tokens, bytes data) public Blocking returns (bool success) {
             allowed[msg.sender][spender] = tokens;
             emit Approval(msg.sender, spender, tokens);
             ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
             return true;
         }
-    }
 
 // Block function to block all other functions
-    function Block() public returns (bool success) {
+    function Block() public isOwner returns (bool success) {
             Block_var = true;
             return Block_var;
         }
+        
 
-// Block function to block all other functions
-    function UnBlock() public returns (bool success) {
+    // Block function to block all other functions
+    function UnBlock() public isOwner returns (bool success) {
             Block_var = false;
             return Block_var;
         }
+
  
  // Fallback function to prevent accounts from directly sending ETH to the contract, this prevents the 
  // users from spending gas on transactions in which they forget to mention the function name.
-    function () public payable {
+    function () public Blocking payable {
         revert();
     }
 }
