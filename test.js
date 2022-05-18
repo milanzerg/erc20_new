f9f703ba490971eddb551cbdb63b10a63ccab1a6
Milanzerg <55619925+milanzerg@users.noreply.github.com>
2022-05-19 00:42:05 +0300

    Update test.js

--- test.js
+++ test.js
@@ -6,11 +6,11 @@ describe("Test 1", function () {
     // Make sure contract is compiled and artifacts are generated
     const metadata = JSON.parse(await remix.call('fileManager', 'getFile', 'contracts/artifacts/Owner.json'))
     const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner()
-    let Value_zero = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
-    let value_zero = await Value_zero.deploy();
+    let Address = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
+    let address_0 = await Address.deploy();
 
-    console.log('value_zero Address: ' + value_zero.address); // logs
-    expect((value_zero.address).length).to.equal(42); // length of the address should equal to 42
+    console.log('value_zero Address: ' + address_0.address); // logs
+    expect((address_0.address).length).to.equal(42); // length of the address should equal to 42
   });
 
 });
